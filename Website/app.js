116e7a781b91c4170dce9bd6e162dd6b4338607c
Ben Tomsett <ben.e.tomsett@gmail.com>
2020-12-07 22:03:01 +0000

    Serve pages without .html extension

--- Website/app.js
+++ Website/app.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
@@ -9,6 +8,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public', {
+    extensions: ['html', 'htm'],
+}));
 
 module.exports = app;
