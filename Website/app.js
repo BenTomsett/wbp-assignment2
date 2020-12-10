14625c15f62e0dba572c0d278bb8197f9cfa8b61
Ben Tomsett <ben.e.tomsett@gmail.com>
2020-12-10 20:46:56 +0000

    Redirect trailing slashes to remove them

--- Website/app.js
+++ Website/app.js
@@ -15,7 +15,16 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public/'));
+
+app.use(function (req, res, next) {
+    if (req.path.substr(-1) == '/' && req.path.length > 1) {
+        let query = req.url.slice(req.path.length)
+        res.redirect(301, req.path.slice(0, -1) + query)
+    } else {
+        next()
+    }
+})
 
 app.use('/register', bookTestRouter);
 app.use('/user', userRouter);
